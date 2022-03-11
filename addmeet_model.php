42620ee54bec241026fcbe1ccdcc228a5e30670d
buranasak <buranasak.s@kkumail.com>
2022-03-11 11:59:47 +0700

    new

--- addmeet_model.php
+++ addmeet_model.php
@@ -3,6 +3,7 @@ include('conn.php');
 
 $title = $_POST['title'];
 $roomid = $_POST['roomid'];
+$roomname = $_POST['roomname'];
 $listname=$_POST['listname'];
 $head = $_POST['head'];
 $numattend = $_POST['numattend'];
@@ -21,5 +22,5 @@ $fileDes = 'upload/' . $newFilename;
 move_uploaded_file($filTmpename,$fileDes);
 $meetfilelocation=$fileDes;
 
-mysqli_query($conn, "insert into meeting(title,roomid,meetfile,numattend,head,end,start) values('$title','$roomid','$meetfilelocation','$numattend','$head','$end','$start')");
+mysqli_query($conn, "insert into meeting(title,roomid,meetfile,numattend,head,end,start,roomname) values('$title','$roomid','$meetfilelocation','$numattend','$head','$end','$start','$roomname')");
 header('location:addmeet.php');
